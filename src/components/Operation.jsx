92390dfe943c75b27e4632bcb1befa5b59c8d220
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-07 10:28:05 +0200

    reack-redux bootstrap

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,14 +1,16 @@
 import {useState} from "react";
-import {store} from "../configureStore/strore.js";
 import {deposit, withdraw} from "../action/accountActions.js";
+import {useDispatch} from "react-redux";
 
 const Operation = () => {
     const [sum, setSum] = useState(0);
+    const dispatch = useDispatch();
+
     return (
-        <div>
-            <button onClick={() => store.dispatch(withdraw(sum))}>Withdraw</button>
-            <input type={'number'} onChange={e =>setSum(+e.target.value)} value={sum} />
-            <button onClick={() => store.dispatch(deposit(sum))}>Deposit</button>
+        <div className={'d-flex justify-content-center'}>
+            <button onClick={() => dispatch(withdraw(sum))} className={'btn btn-primary btn-lg'}>Withdraw</button>
+            <input className={'form-control-lg text-center'} type={'number'} onChange={e =>setSum(+e.target.value)} value={sum} />
+            <button onClick={() => dispatch(deposit(sum))} className={'btn btn-primary btn-lg'}>Deposit</button>
         </div>
     );
 };
